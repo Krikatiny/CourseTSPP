46efb09032f1abf169a7bc1f5fdf45cc72fbccfb
zahar <damerdaner21@gmail.com>
2025-04-15 22:29:18 +0300

    Done dividing input into pieces

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -11,6 +12,12 @@ void searchOperator(char* arr, char* outArr, int position);
 void searchNumberLeft(char* arr, char* outArr, int position);
 int getLength(char* arr);
 
+#define TRUE 1
+#define FALSE 0
+
+int standardOperation = TRUE;
+int pos = 0;
+
 int main()
 {
     char input[100] = " ";
@@ -19,7 +26,6 @@ int main()
     char operation[100] = " ";
     char refactoredInput[100] = " ";
     int length = 0;
-    int pos = 0;
     clearArr(input);
     clearArr(numberRight);
     clearArr(operation);
@@ -27,19 +33,44 @@ int main()
     clearArr(refactoredInput);
 
     getInputInArr(input);
-    clearSpaces(input);
     copyArr(input, refactoredInput);
-    getLength(refactoredInput);
+    clearSpaces(refactoredInput);
+
 
     pos = getOperationPos(refactoredInput, pos);
-    searchNumberRight(refactoredInput, numberRight, pos);
     searchOperator(refactoredInput, operation, pos);
-    searchNumberLeft(refactoredInput, numberLeft, pos);
-
-    printf("%s\n", numberRight);
-    printf("%s\n", operation);
-    printf("%s", numberLeft);
-
+    if (standardOperation == TRUE)
+    {
+        searchNumberLeft(refactoredInput, numberLeft, pos);
+        printf("%s - ", numberLeft);
+        searchNumberRight(refactoredInput, numberRight, pos);
+        printf("%s", operation);
+        printf(" - %s", numberRight);
+        pos++;
+    }
+    else
+    {
+        printf("%s", operation);
+        pos--;
+        searchNumberRight(refactoredInput, numberRight, pos);
+        pos++;
+        printf(" - %s", numberRight);
+        pos++;
+    }
+    length = getLength(refactoredInput);
+    if (pos < length)
+    {
+        for (int i = pos; i < length; i++)
+        {
+            pos = getOperationPos(refactoredInput, pos);
+            searchOperator(refactoredInput, operation, pos);
+            printf(" - %s", operation);
+            searchNumberRight(refactoredInput, numberRight, pos);
+            printf(" - %s", numberRight);
+            pos++;
+            i = pos;
+        }
+    }
     return 0;
 }
 
@@ -50,6 +81,7 @@ void clearArr(char* arr)
     {
         arr[i] = ' ';
     }
+    arr[1] = '\0';
 }
 
 //Get input into array and return length of array
@@ -120,6 +152,7 @@ void searchNumberRight(char* arr, char* outArr, int position)
             break;
         default:
             {
+                return;
             }
         }
     }
@@ -127,9 +160,20 @@ void searchNumberRight(char* arr, char* outArr, int position)
 
 void searchOperator(char* arr, char* outArr, int position)
 {
+    clearArr(outArr);
     //TODO Here I should to change this 0 for sin, cos, tan etc.
-
-    outArr[0] = arr[position];
+    if (arr[position] == 's' && arr[position + 1] == 'i' && arr[position + 2] == 'n')
+    {
+        standardOperation = FALSE;
+        outArr[0] = 's';
+        outArr[1] = 'i';
+        outArr[2] = 'n';
+        pos += 3;
+    }
+    else
+    {
+        outArr[0] = arr[position];
+    }
 }
 
 void searchNumberLeft(char* arr, char* outArr, int position)
@@ -156,6 +200,7 @@ void searchNumberLeft(char* arr, char* outArr, int position)
             break;
         default:
             {
+                return;
             }
         }
     }
@@ -168,16 +213,20 @@ int getOperationPos(char* arr, int position)
         //TODO Define what to do with operators like sin, cos, log
         switch (arr[i])
         {
-        case '+':
-        case '-':
-        case '*':
-        case '/':
-        case '%':
-        case '^':
-            position = i;
-            return position;
+        case '0':
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+            break;
         default:
             {
+                return i;
             }
         }
     }
@@ -188,6 +237,10 @@ int getLength(char* arr)
     int i;
     for (i = 0; arr[i]; i++)
     {
+        if (arr[i] == ' ')
+        {
+            return i;
+        }
     }
     return i;
 }
